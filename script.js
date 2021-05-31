a967c46cdd8002e4df0d57ccd58252c8adcc8067
Flora <81958454+Flora-J@users.noreply.github.com>
2021-05-31 17:29:11 +0200

    commit2

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function footerAlert()
     console.log("clique");
 }
 
-footer.addEventListener("click",posterOnTheConsole);
-console.log("")
+footer.addEventListener("click", footerAlert);
+console.log("");
 
 //fonctionnalité 1 bis :
 function footerAlert2() {
@@ -18,15 +18,43 @@ function footerAlert2() {
       nombre_de_clic++
   })
 }
-footerAlert2()
-console.log("")
+footerAlert2();
+console.log("");
 
 //Fonctionnalité 2 :
+let hamburgerMenu = document.getElementById("navbarHeader");
+let btn = document.querySelector(".navbar-toggler-icon");
+
+function openOrCloseMenu() 
+{
+    hamburgerMenu.classList.toggle("collapse");
+}
+
+btn.addEventListener("click", openOrCloseMenu);
+console.log("");
 
 //Fonctionnalité 3
+const card1 = document.querySelectorAll('div.col-md-4')[0]; 
+const editCard1 = document.querySelector('div.col-md-4 button.btn-outline-secondary')[0]; 
+editCard1.addEventListener('click', function() {     
+  card1.style.color = "red";
+  editCard1.style.color = "red";
+});
+console.log("")
 
 //Fonctionnalité 4
-
+const card2 = document.querySelectorAll('div.col-md-4')[1]; 
+const editCard2 = document.querySelectorAll('div.col-md-4 button.btn-outline-secondary')[1]; 
+function colorGreen() 
+{
+if (secondCard.style.color === 'green'){
+  secondCard.style.color = 'black' ;
+  }else {
+    secondCard.style.color === 'green' 
+  }
+}
+editCard2.addEventListener("click", colorGreen);
+console.log("")
 //Fonctionnalité 5
 
 //Fonctionnalité 6
