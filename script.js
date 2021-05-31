b3008e3877447a36ae9f0e86818fce0d463e186d
Flora <81958454+Flora-J@users.noreply.github.com>
2021-05-31 15:42:17 +0200

    commit1

---
+++ script.js
@@ -0,0 +1,22 @@
+//Fonctionnalité 1
+const footer = document.getElementsByTagName('footer');
+footer.addEventListener('click',function(){
+console.log("clique")
+});
+//Fonctionnalité 1 bis
+
+//Fonctionnalité 2
+
+//Fonctionnalité 3
+
+//Fonctionnalité 4
+
+//Fonctionnalité 5
+
+//Fonctionnalité 6
+
+//Fonctionnalité 7
+
+//Fonctionnalité 8
+
+//Fonctionnalité 9
