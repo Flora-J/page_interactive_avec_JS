281bf56095b429381bf88f5cf6d01759711b9635
Flora <81958454+Flora-J@users.noreply.github.com>
2021-05-31 16:41:44 +0200

    commit2

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-//Fonctionnalité 1
-const footer = document.getElementsByTagName('footer');
-footer.addEventListener('click',function(){
-console.log("clique")
-});
-//Fonctionnalité 1 bis
+//Fonctionnalité 1 :
+let footer = document.querySelector("footer");
 
-//Fonctionnalité 2
+function footerAlert()
+{
+    console.log("clique");
+}
+
+footer.addEventListener("click",posterOnTheConsole);
+console.log("")
+
+//fonctionnalité 1 bis :
+function footerAlert2() {
+  footer = document.querySelector("footer")
+  nombre_de_clic = 1
+  footer.addEventListener("click", function() {
+      alert("clique n° " + nombre_de_clic)
+      nombre_de_clic++
+  })
+}
+footerAlert2()
+console.log("")
+
+//Fonctionnalité 2 :
 
 //Fonctionnalité 3
 
