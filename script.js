72b3de41066a2428a5717ff0d674455626d12022
Flora <81958454+Flora-J@users.noreply.github.com>
2021-06-01 08:27:38 +0200

    commitfinal

--- script.js
+++ script.js
@@ -4,76 +4,81 @@ let footer = document.querySelector("footer");
 function footerAlert()
 {
     console.log("clique");
-}
+};
 
 footer.addEventListener("click", footerAlert);
 console.log("");
+
 //fonctionnalité 1 bis :
 function footerAlert2() {
-  footer = document.querySelector("footer")
-  nombre_de_clic = 1
+  footer = document.querySelector("footer");
+  nombre_de_clic = 1;
   footer.addEventListener("click", function() {
       alert("clique n° " + nombre_de_clic)
       nombre_de_clic++
-  })
-}
+  });
+};
+
 footerAlert2();
 console.log("");
+
 //Fonctionnalité 2 :
 function hamburgerMenu() {
-	menu = document.getElementById("navbarHeader")
-	hambuger = document.querySelector(".navbar-toggler-icon")
+	menu = document.getElementById("navbarHeader");
+	hambuger = document.querySelector(".navbar-toggler-icon");
 	hambuger.addEventListener("click", function () {
 		menu.classList.toggle("collapse")
-	})
-}
-hamburgerMenu()
-console.log("")
+	});
+};
+hamburgerMenu();
+console.log("");
+
 //Fonctionnalité 3
 function redText() {
-	editCardButtonOne = document.querySelectorAll("div.col-md-4")[0]
-	textCardOne = document.querySelectorAll("div.col-md-4 button.btn-outline-secondary")[0]
+	editCardButtonOne = document.querySelectorAll("div.col-md-4")[0];
+	textCardOne = document.querySelectorAll("div.col-md-4 button.btn-outline-secondary")[0];
 	editCardButtonOne.addEventListener("click", function() {
     textCardOne.style.color = "red"
-	})
-}
-redText()
-console.log("")
+	});
+};
+
+redText();
+console.log("");
+
 //Fonctionnalité 4
 function greenText() {
-	editCardButtonTwo = document.querySelectorAll("div.col-md-4")[1]
-	textCardTwo = document.querySelectorAll("div.col-md-4 button.btn-outline-secondary")[1]
+	editCardButtonTwo = document.querySelectorAll("div.col-md-4")[1];
+	textCardTwo = document.querySelectorAll("div.col-md-4 button.btn-outline-secondary")[1];
 	i = 1
 	editCardButtonTwo.addEventListener("click", function() {
 		i++
     if (textCardTwo.style.color === "green")
-    {
-      textCardTwo.style.color = "";
-    }
+      {textCardTwo.style.color = "";}
     else
-    {
-      textCardTwo.style.color = "green";
-    }
-	})
-}
-greenText()
-console.log("")
+      {textCardTwo.style.color = "green";}
+	});
+};
+
+greenText();
+console.log("");
+
 //Fonctionnalité 5 
 function nuclearPage() {
-	navbar = document.querySelector("div.navbar")
-	bootstrap = document.querySelector("head").querySelector("link")
-	i = 1
+	navbar = document.querySelector("div.navbar");
+	bootstrap = document.querySelector("head").querySelector("link");
+	i = 1;
 	navbar.addEventListener("dblclick", function () {
 		i++
-		if (document.getElementsByTagName("link")[0].rel == "stylesheet")           //if stylesheet is on, we turn it off
-    document.getElementsByTagName("link")[0].rel = "styleDisabled";
-  else
-    document.getElementsByTagName("link")[0].rel = "stylesheet";
-	})
-}
+		if (document.getElementsByTagName("link")[0].rel == "stylesheet")          
+      document.getElementsByTagName("link")[0].rel = "styleDisabled";
+    else
+      document.getElementsByTagName("link")[0].rel = "stylesheet";
+	});
+};
 
-nuclearPage()
-console.log("")
+nuclearPage();
+console.log("");
+
 //Fonctionnalité 6
 let cards = document.getElementsByClassName("card");
 let button_view = document.getElementsByClassName("btn-success");
@@ -83,23 +88,22 @@ for(let i = 0; i < cards.length; i++)
     function view()
     {
         if (cards[i].getElementsByClassName("card-text")[0].style.display != "none")
-        {
-            cards[i].getElementsByClassName("card-text")[0].style.display = "none";
-            cards[i].getElementsByClassName("card-img-top")[0].style.width = "20%";
-        }
+          {cards[i].getElementsByClassName("card-text")[0].style.display = "none";
+          cards[i].getElementsByClassName("card-img-top")[0].style.width = "20%";}
         else
-        {
-            cards[i].getElementsByClassName("card-text")[0].style.display = "block";
-            cards[i].getElementsByClassName("card-img-top")[0].style.width = "100%";
-        }
+          {cards[i].getElementsByClassName("card-text")[0].style.display = "block";
+          cards[i].getElementsByClassName("card-img-top")[0].style.width = "100%";}
     }
     button_view[i].addEventListener("mouseover", view);
-}
-console.log("")
+};
+
+console.log("");
+
 //Fonctionnalité 7
 const buttonNext = document.querySelector('main section p a.btn-secondary'); 
 const cards2 = document.querySelectorAll('div.col-md-4'); 
 const divcards = document.querySelector('div.album div.container div.row');
+
 let card_1 = divcards.children[0];
 let card_6 = divcards.children[5];
 
@@ -108,7 +112,9 @@ buttonNext.addEventListener('click', function() {
   card_1 = divcards.children[0];
   card_6 = divcards.children[5];
 }); 
+
 console.log("")
+
 //Fonctionnalité 8
 const buttonPrevious = document.querySelector('main section p a.btn-primary'); 
 card_1 = divcards.children[0];
@@ -120,12 +126,16 @@ buttonPrevious.addEventListener('click', function(event) {
   card_1 = divcards.children[0];
   card_6 = divcards.children[5];
 }); 
+
 console.log("")
+
 //Fonctionnalité 9
 let logo = document.getElementsByClassName('navbar-brand')[0];
+
 logo.addEventListener('keypress', function(e) {
   let body = document.getElementsByTagName('body')[0];
   body.classList = "";
+  
   switch (e.key) {
     case 'a':
       body.classList.add("col-4");
